a5744a9a6d5b21454e000120d5242491a9530068
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-09-24 13:38:26 +0200

    Deleting all default files

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#0a7ea4';
+const tintColorLight = '#4a3228';
 const tintColorDark = '#fff';
 
 export const Colors = {
@@ -16,7 +16,7 @@ export const Colors = {
     tabIconSelected: tintColorLight,
   },
   dark: {
-    text: '#ECEDEE',
+    text: '#ECEDEE', 
     background: '#151718',
     tint: tintColorDark,
     icon: '#9BA1A6',
