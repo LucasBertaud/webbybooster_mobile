7c08f9d0978122e347937f25825df634f63fff75
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-09-26 10:28:14 +0200

    Adding 4 others school

--- mock/organizations_mock.ts
+++ mock/organizations_mock.ts
@@ -1,6 +1,10 @@
 import Organization from "@/entities/organization";
 
-export const OrganizationsMock : Array<Organization> = [
-  new Organization(1, "Open Classrooms", "https://blog.openclassrooms.com/wp-content/uploads/2018/05/OC.png", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
-  new Organization(2, "CESI", "https://s3-eu-west-1.amazonaws.com/assets.atout-on-line.com/images/ingenieur/Logos_Ecoles/2022_2025/cesi-300.jpg")
+export const OrganizationsMock: Array<Organization> = [
+    new Organization(1, "Open Classrooms", "https://blog.openclassrooms.com/wp-content/uploads/2018/05/OC.png", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
+    new Organization(2, "CESI", "https://s3-eu-west-1.amazonaws.com/assets.atout-on-line.com/images/ingenieur/Logos_Ecoles/2022_2025/cesi-300.jpg", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
+    new Organization(3, "MBWay", "https://storage.letudiant.fr/osp/cards/465/7ef288c1e52bf4fb7e3ec2486fc54ed38b0caaed.png", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
+    new Organization(4, "MyDigitalSchool", "https://www.mydigitalschool.com/sites/default/files/2023-03/logo-web.png", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
+    new Organization(5, "Ipac Factory", "https://www.ipacbachelorfactory.com/sites/default/files/2023-09/LOGO_IPAC_BACHELOR_FACTORY_2018_QUADRI_6.jpg", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
+    new Organization(6, "Iscom", "https://www.iscom.fr/sites/default/files/2024-09/ISCOM_Noir-cote-cote-InCommWeTrust%202.png", "01 80 88 80 30", "2 Cr de l'île Louviers, 75004 Paris"),
 ];
