5ff13b8e6b5287bdbc6093e635b13d09e5b8eaa1
Lucas Madranges <lucas.madranges.pro@icloud.com>
2024-09-26 15:47:18 +0200

    fix: AgendaScreen.tsx

--- components/AgendaScreen.tsx
+++ components/AgendaScreen.tsx
@@ -1,8 +1,7 @@
-import React, {Component} from 'react';
-import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {Agenda, DateData, AgendaEntry, AgendaSchedule} from 'react-native-calendars';
-import TestIDS from "@/app/testIDS";
-import TitlePage from "@/components/titles/TitlePage";
+import React, {Component} from "react";
+import {Alert, StyleSheet, Text, TouchableOpacity, View} from "react-native";
+import {Agenda, AgendaEntry, AgendaSchedule, DateData} from "react-native-calendars";
+import TestIDS from "@/constants/testIDS";
 
 interface State {
     itemsAgenda?: AgendaSchedule;
@@ -10,23 +9,31 @@ interface State {
 
 export default class AgendaScreen extends Component<State> {
     state: State = {
-        itemsAgenda: undefined
+        itemsAgenda: undefined,
     };
 
     render() {
         return (
-            <View style={{flex: 1, width: '100%'}}>
-            <Agenda
-                testID={TestIDS.agenda.CONTAINER}
-                items={this.state.itemsAgenda}
-                loadItemsForMonth={this.events}
-                selected={new Date().toISOString().split('T')[0]}
-                renderItem={this.renderItem}
-                renderEmptyDate={this.renderEmptyDate}
-                rowHasChanged={this.rowHasChanged}
-                showClosingKnob={true}
-                theme={{ dotColor: '#ba856f', agendaDayTextColor: '#ba856f', agendaDayNumColor: '#ba856f', agendaTodayColor: '#ba856f', agendaKnobColor: '#ba856f', selectedDayBackgroundColor: '#ba856f', todayTextColor: '#ba856f' }}
-            />
+            <View style={{flex: 1, width: "100%"}}>
+                <Agenda
+                    testID={TestIDS.agenda.CONTAINER}
+                    items={this.state.itemsAgenda}
+                    loadItemsForMonth={this.events}
+                    selected={new Date().toISOString().split("T")[0]}
+                    renderItem={this.renderItem}
+                    renderEmptyDate={this.renderEmptyDate}
+                    rowHasChanged={this.rowHasChanged}
+                    showClosingKnob={true}
+                    theme={{
+                        dotColor: "#ba856f",
+                        agendaDayTextColor: "#ba856f",
+                        agendaDayNumColor: "#ba856f",
+                        agendaTodayColor: "#ba856f",
+                        agendaKnobColor: "#ba856f",
+                        selectedDayBackgroundColor: "#ba856f",
+                        todayTextColor: "#ba856f",
+                    }}
+                />
                 <TouchableOpacity>
                     <Text>+</Text>
                 </TouchableOpacity>
@@ -38,27 +45,35 @@ export default class AgendaScreen extends Component<State> {
         const items: AgendaSchedule = {};
         setTimeout(() => {
 
-            items[new Date().toISOString().split('T')[0]] = [
-                {name: 'Cours avec les M1 - Dev', height: 75, day: new Date().toString().split('GMT')[0].trimEnd().slice(0,-3)},
-                {name: 'Cours avec les M2 - UX/UI', height: 75, day: (new Date().toString().split('GMT')[0]).trimEnd().slice(0,-3)},
+            items[new Date().toISOString().split("T")[0]] = [
+                {
+                    name: "Cours avec les M1 - Dev",
+                    height: 75,
+                    day: new Date().toString().split("GMT")[0].trimEnd().slice(0, -3),
+                },
+                {
+                    name: "Cours avec les M2 - UX/UI",
+                    height: 75,
+                    day: (new Date().toString().split("GMT")[0]).trimEnd().slice(0, -3),
+                },
             ];
 
             for (let i = -15; i < 85; i++) {
 
-                            const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-                            const strTime = this.timeToString(time);
+                const time = day.timestamp + i * 24 * 60 * 60 * 1000;
+                const strTime = this.timeToString(time);
 
-                            if (!items[strTime]) {
-                                items[strTime] = [];
-                            }
-                        }
+                if (!items[strTime]) {
+                    items[strTime] = [];
+                }
+            }
             this.setState({itemsAgenda: items});
         }, 1000);
-    }
+    };
 
     renderItem = (reservation: AgendaEntry, isFirst: boolean) => {
         const fontSize = isFirst ? 20 : 14;
-        const color = isFirst ? 'red' : 'black';
+        const color = isFirst ? "red" : "black";
 
         return (
             <TouchableOpacity
@@ -67,7 +82,7 @@ export default class AgendaScreen extends Component<State> {
                 onPress={() => Alert.alert(reservation.name)}
             >
                 <Text style={{fontSize, color, paddingBottom: 5}}>{reservation.name}</Text>
-                <Text style={{fontSize: 12, color: 'grey', paddingTop: 5}}>{reservation.day}</Text>
+                <Text style={{fontSize: 12, color: "grey", paddingTop: 5}}>{reservation.day}</Text>
             </TouchableOpacity>
         );
     };
@@ -86,22 +101,22 @@ export default class AgendaScreen extends Component<State> {
 
     timeToString(time: number) {
         const date = new Date(time);
-        return date.toISOString().split('T')[0];
+        return date.toISOString().split("T")[0];
     }
 }
 
 const styles = StyleSheet.create({
     item: {
-        backgroundColor: 'white',
+        backgroundColor: "white",
         flex: 1,
         borderRadius: 5,
         padding: 10,
         marginRight: 10,
-        marginTop: 17
+        marginTop: 17,
     },
     emptyDate: {
         height: 15,
         flex: 1,
-        paddingTop: 30
+        paddingTop: 30,
     },
 });
