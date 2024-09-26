b813ff656088facb9e07aaf92fe0f667955c4d47
edixoo <paul1pruvost@gmail.com>
2024-09-26 15:35:19 +0200

    feat(): add intervention page with calendar

--- app/(tabs)/intervention.tsx
+++ app/(tabs)/intervention.tsx
@@ -1,7 +1,14 @@
+import {View} from "react-native";
+import AgendaScreen from "@/components/AgendaScreen";
+import FloatingButton from "@/components/button/FloatingButton";
 import TitlePage from "@/components/titles/TitlePage";
 
 export default function InterventionPage() {
     return (
+        <View style={{flex: 1, width: '100%'}}>
         <TitlePage title="Interventions"/>
+            <AgendaScreen/>
+            <FloatingButton onPress={() => {console.log('pressed')}}/>
+        </View>
     );
 }
