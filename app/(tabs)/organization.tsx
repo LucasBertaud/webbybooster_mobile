b15539450b8fbc23bc92441b2be1076c1d821817
pagiestm <pagiestm@gmail.com>
2024-09-26 11:39:57 +0200

    fix(organization.tsx): added margin right and left and page structure

--- app/(tabs)/organization.tsx
+++ app/(tabs)/organization.tsx
@@ -1,8 +1,8 @@
 import TitlePage from "@/components/titles/TitlePage";
 import OrganizationCard from "@/components/cards/OrganizationCard";
 import React from "react";
-import {ScrollView} from "react-native";
-import {SheetProvider} from "react-native-actions-sheet";
+import { View, ScrollView, StyleSheet } from "react-native";
+import { SheetProvider } from "react-native-actions-sheet";
 import TimerBlock from "@/components/blocks/TimerBlock";
 import ShortcutsBlock from "@/components/blocks/ShortcutsBlock";
 
@@ -10,18 +10,28 @@ export default function OrganizationPage() {
     return (
         <>
             <SheetProvider>
-                <ScrollView style={{marginBottom: 12}}>
-                    <TitlePage title="Organisme de formations"/>
+                <ScrollView style={{ marginBottom: 12 }}>
+                    <TitlePage title="Organisme de formations" />
+                    <TimerBlock />
+                    <ShortcutsBlock />
+                    <View style={styles.container}>
                     <OrganizationCard id={1}></OrganizationCard>
                     <OrganizationCard id={2}></OrganizationCard>
                     <OrganizationCard id={3}></OrganizationCard>
                     <OrganizationCard id={4}></OrganizationCard>
                     <OrganizationCard id={5}></OrganizationCard>
                     <OrganizationCard id={6}></OrganizationCard>
-                    <TimerBlock/>
-                    <ShortcutsBlock/>
-                </ScrollView>
-            </SheetProvider>
+                </View>
+            </ScrollView>
+        </SheetProvider >
         </>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginLeft: 14,
+        marginRight: 14,
+    },
+});
